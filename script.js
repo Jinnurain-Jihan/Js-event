9c95e562427c22f7d14b466e76f0e62471350798
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-10-20 09:11:13 +0600

    event is added

---
+++ script.js
@@ -0,0 +1,12 @@
+function onClick(){
+    var h2=document.getElementById("h2");
+    h2.style.color="orange";
+}
+function dblClick(){
+    alert("hey");
+}
+function mouseOver(){
+    var p=document.getElementById('p');
+    p.style.fontSize="20px";
+    p.style.backgroundColor="blue";
+}
